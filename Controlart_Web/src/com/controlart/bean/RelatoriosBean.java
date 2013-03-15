fd53a78dbe8118e11108ff8ea82dd61be8eebe67
Ramon Ramos <rbr90graus@yahoo.com.br>
2013-03-15 00:47:34 -0300

    Inicio da inclusao do modulo dos relatorios

---
+++ Controlart_Web/src/com/controlart/bean/RelatoriosBean.java
@@ -0,0 +1,16 @@
+package com.controlart.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name="relatoriosBean")
+@ViewScoped
+public class RelatoriosBean extends ControlArtBean {
+
+	private static final long serialVersionUID = 1L;
+	
+	public void gerarRelatorio(){
+		
+	}
+
+}
