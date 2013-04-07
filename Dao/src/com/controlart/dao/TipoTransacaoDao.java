ad0551f1391d75b63970f3e46ba25c249534edf0
Ramon Ramos <rbr90graus@yahoo.com.br>
2013-04-07 03:19:29 -0300

    Tratando conflito

--- Dao/src/com/controlart/dao/TipoTransacaoDao.java
+++ Dao/src/com/controlart/dao/TipoTransacaoDao.java
@@ -14,13 +14,13 @@ import com.controlart.transfer.TipoTransacaoT;
 public class TipoTransacaoDao {
 	private Connection connection;
 
-<<<<<<< HEAD
-	private static final String SQL_CONSULT_ALL = "SELECT ttt.* FROM tb_tipo_transacao ttt ORDER BY ttt.nm_tipo_transacao";
+
+	private static final String SQL_CONSULT_ALL_FOR_VIEW = "SELECT ttt.id_tipo_transacao, ttt.nm_tipo_transacao, ttt.in_ativo FROM tb_tipo_transacao ttt ORDER BY ttt.nm_tipo_transacao";
 	private static final String SQL_CONSULT_TIPO_TRANSACAO_COM_DEVOLUCAO = "SELECT ttt.* FROM tb_tipo_transacao ttt WHERE ttt.id_tipo_transacao IN (3,5) ORDER BY ttt.nm_tipo_transacao";
 	private static final String SQL_CONSULT_TIPO_TRANSACAO_ENTRADA = "SELECT ttt.* FROM tb_tipo_transacao ttt WHERE ttt.tp_operacao='E' ORDER BY ttt.nm_tipo_transacao";
-=======
-	private static final String SQL_CONSULT_ALL_FOR_VIEW = "SELECT ttt.id_tipo_transacao, ttt.nm_tipo_transacao, ttt.in_ativo FROM tb_tipo_transacao ttt ORDER BY ttt.nm_tipo_transacao";
->>>>>>> 95660fe66028c907f511b722190985e9ced2350f
+
+	
+
 
 	public TipoTransacaoDao() throws SQLException {
 		connection = ConnFactory.getConnection();
@@ -54,7 +54,6 @@ public class TipoTransacaoDao {
 		}
 	}
 
-<<<<<<< HEAD
 	public List<TipoTransacaoT> consultTipoTransacaoComDevolucao() throws SQLException {
 		PreparedStatement pStmt = null;
 		ResultSet rs = null;
@@ -64,7 +63,7 @@ public class TipoTransacaoDao {
 
 			rs = pStmt.executeQuery();
 
-			return resultsetToObjectT(rs);
+			return resultsetToObject(rs);
 		} finally {
 			DaoUtils.closePreparedStatement(pStmt);
 			DaoUtils.closeConnection(connection);
@@ -80,15 +79,13 @@ public class TipoTransacaoDao {
 
 			rs = pStmt.executeQuery();
 
-			return resultsetToObjectT(rs);
+			return resultsetToObject(rs);
 		} finally {
 			DaoUtils.closePreparedStatement(pStmt);
 			DaoUtils.closeConnection(connection);
 		}
 	}
-
-	private List<TipoTransacaoT> resultsetToObjectT(ResultSet rs)
-=======
+	
 	/*
 	 * Objetivo: Método utilizado para mapear dados de um ResultSet (Que
 	 * armazena resultados de consultas em uma Base de Dados) em informações de
@@ -103,7 +100,6 @@ public class TipoTransacaoDao {
 	 */
 
 	private List<TipoTransacaoT> resultsetToObject(ResultSet rs)
->>>>>>> 95660fe66028c907f511b722190985e9ced2350f
 			throws SQLException {
 		List<TipoTransacaoT> listaTipoTransacaoT = new ArrayList<TipoTransacaoT>(
 				0);
